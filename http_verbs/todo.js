df64af10439c289b49724e84b9855cb6bfcb1a66
Chetan <chetan@vouchercodesuae.com>
2014-07-21 19:50:17 +0530

    Static file server, readStream

--- http_verbs/todo.js
+++ http_verbs/todo.js
@@ -6,6 +6,7 @@ var server  = http.createServer(function(req,res){
 	console.log(req.method);
 	switch(req.method){
 		case 'POST':
+			// POST curl command--> curl -d "buy node in action" http://localhost:3000
 			var item='';
 			req.setEncoding('utf8');
 			req.on('data',function(chunk){
@@ -17,6 +18,8 @@ var server  = http.createServer(function(req,res){
 			});
 			break;
 		case 'GET':
+			//GET curl command--> curl http://localhost:3000
+			
 			/*items.forEach(function(item,i){
 				res.write(i+')'+item+'\n');
 			});*/
@@ -28,6 +31,22 @@ var server  = http.createServer(function(req,res){
 			res.end(body);
 			console.log('Request took:', new Date() - start, 'ms');
 			
+			break;
+		case "PUT":
+			req.on("data",function(chunk){
+				var path=url.parse(req.url).pathname;
+				var i =	parseInt(path.slice(1),10);
+				items[i]=chunk;
+				
+			});
+			req.on('end',function(){
+				var body = items.map(function(item, i){
+				return i + ') ' + item;
+				}).join('\n');
+				res.setHeader('Content-Length', Buffer.byteLength(body));
+				res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
+				res.end(body);
+			});
 			break;
 		case 'DELETE':
 			var path=url.parse(req.url).pathname;
